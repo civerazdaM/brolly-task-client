ad83691e3aa5b874b7a9e3b202f99fe851f5a07a
Stefan Madzarevic <stev291@gmail.com>
2017-12-13 09:35:40 +0100

    Added validation for username and password on login page

--- App/containers/LoginPage/components/LoginComponent/index.js
+++ App/containers/LoginPage/components/LoginComponent/index.js
@@ -6,25 +6,47 @@ import {StyleSheet, Text, TextInput, TouchableHighlight, View} from "react-nativ
 class LoginComponent extends Component {
     state = {
         username: '',
-        password: ''
+        usernameError: '',
+        password: '',
+        passwordError: ''
     };
 
     onLoginPressed = () => {
         let username = this.state.username;
         let password = this.state.password;
-        this.props.onLogin({username, password});
+        let hasError = false;
+        if(!username) {
+            hasError = true;
+            this.setState({usernameError: 'Username is required!'});
+        }
+        if(!password) {
+            hasError = true;
+            this.setState({passwordError: 'Password is required!'});
+        }
+        if(!hasError) {
+            this.props.onLogin({username, password});
+        }
     };
 
     changeUsername = (event) => {
-        this.setState({username: event.nativeEvent.text});
+        let username = event.nativeEvent.text;
+        let usernameError = '';
+        if(!username) {
+            usernameError = 'Username is required!';
+        }
+        this.setState({username, usernameError});
     };
 
     changePassword = (event) => {
-        this.setState({password: event.nativeEvent.text});
+        let password = event.nativeEvent.text;
+        let passwordError = '';
+        if(!password) {
+            passwordError = 'Password is required!';
+        }
+        this.setState({password, passwordError});
     };
 
     render() {
-        console.log(this.props);
         const { from } = this.props.location.state || { from: { pathname: '/locker' } }
         const { isAuthenticated } = this.props;
 
@@ -36,11 +58,13 @@ class LoginComponent extends Component {
 
         return (
             <View style={styles.container}>
+                <Text>{this.state.usernameError}</Text>
                 <TextInput
                     style={styles.input}
                     onChange={this.changeUsername}
                     placeholder={'Username'}
                 />
+                <Text>{this.state.passwordError}</Text>
                 <TextInput
                     style={styles.input}
                     onChange={this.changePassword}
